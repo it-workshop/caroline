ba38f6d0f2b365c49dd88e504da3e0e09ee59f75
Glazachev Vladimir <glazachev.vladimir@gmail.com>
2014-04-21 17:31:17 +0400

    initial depthbuilder commit

---
+++ core/depth_map_builder.cc
@@ -0,0 +1,14 @@
+// Copyright (c) 2014 The Caroline authors. All rights reserved.
+// Use of this source file is governed by a MIT license that can be found in the
+// LICENSE file.
+// Author: Glazachev Vladimir <glazachev.vladimir@gmail.com>
+
+#include "depth_map_builder.h"
+
+namespace core {
+
+DepthMapBuilder::DepthMapBuilder()
+{
+}
+
+}  // namespace core
---
+++ core/depth_map_builder.h
@@ -0,0 +1,19 @@
+// Copyright (c) 2014 The Caroline authors. All rights reserved.
+// Use of this source file is governed by a MIT license that can be found in the
+// LICENSE file.
+// Author: Glazachev Vladimir <glazachev.vladimir@gmail.com>
+
+#ifndef CORE_DEPTH_MAP_BUILDER_H_
+#define CORE_DEPTH_MAP_BUILDER_H_
+
+namespace core {
+
+class DepthMapBuilder
+{
+public:
+  DepthMapBuilder();
+};
+
+}  // namespace core
+
+#endif // CORE_DEPTH_MAP_BUILDER_H_
