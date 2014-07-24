7d002f73189ef56e5ac6454158df73fedb60430a
VladimirGl <glazachev.vladimir@gmail.com>
2014-07-24 19:54:35 +0400

    #68: rectification files added

---
+++ core/rectification.h
@@ -0,0 +1,18 @@
+// Copyright (c) 2014 The Caroline authors. All rights reserved.
+// Use of this source file is governed by a MIT license that can be found in the
+// LICENSE file.
+// Author: Glazachev Vladimir <glazachev.vladimir@gmail.com
+
+#ifndef CORE_RECTIFICATION_H_
+#define CORE_RECTIFICATION_H_
+
+namespace core {
+
+class Rectification {
+ public:
+  Rectification();
+};
+
+}  // namespace core
+
+#endif  // CORE_RECTIFICATION_H_
