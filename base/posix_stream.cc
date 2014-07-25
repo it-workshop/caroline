01ecce6d3925add085e22adc17640f41733b0a37
VladimirGl <glazachev.vladimir@gmail.com>
2014-07-25 16:26:01 +0400

    #70: empty posix stream impl

---
+++ base/posix_stream.cc
@@ -0,0 +1,13 @@
+// Copyright (c) 2014 The Caroline authors. All rights reserved.
+// Use of this source file is governed by a MIT license that can be found in the
+// LICENSE file.
+// Author: Glazachev Vladimir <glazachev.vladimir@gmail.com>
+
+#include "base/posix_stream.h"
+
+namespace base {
+
+POSIXStream::POSIXStream() {
+}
+
+}  // namespace base
---
+++ base/posix_stream.h
@@ -0,0 +1,18 @@
+// Copyright (c) 2014 The Caroline authors. All rights reserved.
+// Use of this source file is governed by a MIT license that can be found in the
+// LICENSE file.
+// Author: Glazachev Vladimir <glazachev.vladimir@gmail.com>
+
+#ifndef BASE_POSIX_STREAM_H_
+#define BASE_POSIX_STREAM_H_
+
+namespace base {
+
+class POSIXStream {
+ public:
+  POSIXStream();
+};
+
+}  // namespace base
+
+#endif  // BASE_POSIX_STREAM_H_
