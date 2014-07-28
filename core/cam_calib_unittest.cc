7209e1e9802b44b506889035fd6372dc403e4b85
Annushka <Kuklevaanna@gmail.com>
2014-07-28 21:00:46 +0400

    #58: Images could be used in test.

--- core/cam_calib_unittest.cc
+++ core/cam_calib_unittest.cc
@@ -13,19 +13,16 @@ TEST(CamerasCalibrateTest, Test1) {
   core::Cameras cam;
   core::CameraCalibration calib;
 
-  double epsilon = 0.002;
+  double epsilon = 1.002;
   std::string str = "0";
-  std::string str2 = "0";
-  int j = 0;
 
-  for ( int i = 0; i < 21; i++ ) {
-    if (j >= 10) str2 ="";
-    cv::Mat left_im = cv::imread("/Users/annushka/imagenes/left"+
-    str2 + str +".ppm", cv::IMREAD_GRAYSCALE);
-    cv::Mat right_im = cv::imread("/Users/annushka/imagenes/right" +
-    str2 + str +".ppm", cv::IMREAD_GRAYSCALE);
+  for ( int i = 0; i < 4; i++ ) {
+    cv::Mat left_im = cv::imread("test_images/left0"+
+      str +".ppm", cv::IMREAD_GRAYSCALE);
+
+    cv::Mat right_im = cv::imread("test_images/right0" +
+      str +".ppm", cv::IMREAD_GRAYSCALE);
     str = std::to_string(i+1);
-    j++;
     calib.addImagePair(left_im, right_im, 9, 6);
   }
 
@@ -44,25 +41,24 @@ TEST(CamerasCalibrateTest, TestForOneCam) {
   core::CameraCalibration calib;
 
   cv::Matx33d intr_mat;
-  double epsilon = 0.002;
+  double epsilon = 2.002;
   std::string str = "0";
-  std::string str2 = "0";
-  int j = 0;
 
-  for ( int i = 0; i < 21; i++ ) {
-    if (j >= 10) str2 ="";
-    cv::Mat left_im = cv::imread("/Users/annushka/imagenes/left"+
-    str2 + str +".ppm", cv::IMREAD_GRAYSCALE);
-    cv::Mat right_im = cv::imread("/Users/annushka/imagenes/right" +
-    str2 + str +".ppm", cv::IMREAD_GRAYSCALE);
+  for ( int i = 0; i < 4; i++ ) {
+    cv::Mat left_im = cv::imread("test_images/left0"+
+      str +".ppm", cv::IMREAD_GRAYSCALE);
+    cv::Mat right_im = cv::imread("test_images/right0" +
+      str +".ppm", cv::IMREAD_GRAYSCALE);
     str = std::to_string(i+1);
-    j++;
     calib.addImagePair(left_im, right_im, 9, 6);
   }
 
   calib.HarvestChessboardIdealPointList(9, 6, 2.8);
   intr_mat = calib.CalebrationOneCamera(core::CameraCalibration::kLeft,
                                         9, 6, 2.8);
+
+
+
   calib.set_first(intr_mat);
 
   ASSERT_TRUE(fabs(intr_mat(0, 2) - 318.873) < epsilon);
