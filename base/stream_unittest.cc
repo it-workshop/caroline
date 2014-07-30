d95a6ebc41773445627c7f87330c1e8867fbbaff
PilgrimViis <bormotovk@gmail.com>
2014-07-30 16:00:15 +0400

    Fix ports problem

--- base/stream_unittest.cc
+++ base/stream_unittest.cc
@@ -2,28 +2,47 @@
 // Use of this source file is governed by a MIT license that can be found in the
 // LICENSE file.
 /// @author Konstantin Bormotov <bormotovk@gmail.com>
+/// @author Vladimir Glazachev <glazachev.vladimir@gmail.com>
 
 #include "gtest/gtest.h"
 #include "base/stream.h"
-#include "base/logging.h"
 
-TEST (StreamTest, TCPOpenTest) {
-  base::Logger::GetInstance()->Init(std::string(), base::Logger::Level::LOG_ERR);
-  std::unique_ptr<base::Stream> stream = base::Stream::Open("tcp-bind://127.0.0.1:3452", base::Stream::kReadWrite);
-  std::unique_ptr<base::Stream> stream2 = base::Stream::Open("tcp://127.0.0.1:3452", base::Stream::kReadWrite);
+TEST(StreamTest, TCPOpenTest) {
+  std::unique_ptr<base::Stream> stream;
+  std::unique_ptr<base::Stream> stream2;
+
+  for (int i = 3500; i < 3510; i++) {
+    stream = base::Stream::Open("tcp-bind://127.0.0.1:" + std::to_string(i),
+      base::Stream::kReadWrite);
+    stream2 = base::Stream::Open("tcp://127.0.0.1:" + std::to_string(i),
+      base::Stream::kReadWrite);
+
+    if ((stream != nullptr) || (stream2 != nullptr)) {
+      break;
+    }
+  }
+
+  ASSERT_NE(stream, nullptr);
+  ASSERT_NE(stream2, nullptr);
 
   std::string sent_message("Hello, 'm test message");
-  std::unique_ptr<char[]> recieve_buffer(new char[sent_message.size()+1]);
+  std::unique_ptr<char[]> recieve_buffer(new char[sent_message.size() + 1]);
+
   size_t transmitted = 0;
   do {
-    transmitted += stream2->Write(sent_message.c_str() + transmitted, sent_message.size() - transmitted);
+    transmitted += stream2->Write(sent_message.c_str() + transmitted,
+      sent_message.size() - transmitted);
   } while (transmitted != sent_message.size());
 
   size_t received = 0;
   do {
-    received += stream->Read(recieve_buffer.get() + received, sent_message.size() - received);
+    received += stream->Read(recieve_buffer.get() + received,
+      sent_message.size() - received);
   } while (received != sent_message.size());
   recieve_buffer[received] = '\0';
 
   EXPECT_STREQ(sent_message.c_str(), recieve_buffer.get());
+
+  stream2->Close();
+  stream->Close();
 }
