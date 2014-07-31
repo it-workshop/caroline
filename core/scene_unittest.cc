9f970bd7b91ac5d2e1c740bffb29dde41c53d5e5
MikhailMlodik <mlodik_m@mail.ru>
2014-07-31 22:26:22 +0400

    #65: All is working. Not supporting deletion of coincident faces and cv::point3d.

--- core/scene_unittest.cc
+++ core/scene_unittest.cc
@@ -116,11 +116,11 @@ TEST(SceneTest, ObjectScalingTest) {
   EXPECT_TRUE((Line.Vertexes()[1].z() - 2) < precision);
 }
 
-TEST(SceneTest, MeshSortingTest) {
+TEST(SceneTest, MeshMergingTest) {
   core::Point3D point1(10, 0, 0);
   core::Point3D point2(2, 1, 0);
-  core::Point3D point3(1, 0, 1);
-  core::Point3D point4(0, 0, 0);
+  core::Point3D point3(3, 0, 1);
+  core::Point3D point4(5, 0, 0);
   core::Mesh mesh;
   mesh.AddVertex(point1);
   mesh.AddVertex(point2);
@@ -150,5 +150,9 @@ TEST(SceneTest, MeshSortingTest) {
   std::vector<core::Point3D> some_vertices;
   some_vertices = GluedMesh.Vertexes();
   EXPECT_EQ(GluedMesh.Vertexes().size(), 7);
+<<<<<<< HEAD
 >>>>>>> #65: Working merge of mesh and scene. #52 bug is fixed.
+=======
+  EXPECT_EQ(GluedMesh.Faces().at(4).Point1(), 0);
+>>>>>>> #65: All is working. Not supporting deletion of coincident faces and cv::point3d.
 }
