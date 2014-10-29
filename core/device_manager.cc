09499c495ed8e83eb27044dcefcddcb9735b5288
Aleksandr Derbenev <ya.alex-ac@yandex.ru>
2014-10-29 11:37:45 +0300

    CAROLINE-31: Add interface for device manager.

---
+++ core/device_manager.cc
@@ -0,0 +1,14 @@
+// Copyright (c) 2014 The Caroline authors. All rights reserved.
+// Use of this source file is governed by a MIT license that can be found in the
+// LICENSE file.
+/// @author Aleksandr Derbenev <alex@technoworks.ru>
+
+#include "core/device_manager.h"
+
+namespace core {
+
+DeviceManager::DeviceManager() {}
+DeviceManager::~DeviceManager() {}
+
+}  // namespace core
+
