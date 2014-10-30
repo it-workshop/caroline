e5693a1c4fb8a1e1caf2277f850dab145e482e39
Alexander <alex-ac@users.noreply.github.com>
2014-10-30 21:54:15 +0300

    Merge pull request #118 from it-workshop/wp/CAROLINE-31/1
    CAROLINE-31: Add interface for device manager.
    (cherry picked from commit 2141afae97a3bcf9e9a7663efa016d488e69033e)

---
+++ core/device_manager.h
@@ -0,0 +1,47 @@
+// Copyright (c) 2014 The Caroline authors. All rights reserved.
+// Use of this source file is governed by a MIT license that can be found in the
+// LICENSE file.
+/// @author Aleksandr Derbenev <alex@technoworks.ru>
+
+#ifndef CORE_DEVICE_MANAGER_H_
+#define CORE_DEVICE_MANAGER_H_
+
+#include <memory>
+#include <string>
+#include <vector>
+
+#include "base/macro.h"
+
+namespace core {
+
+/// Class that enumerates video capture devices connected to the computer.
+class DeviceManager {
+ public:
+  DeviceManager();
+  virtual ~DeviceManager();
+
+  /// Fabric method. Returns platform-specific implementation.
+  static std::unique_ptr<DeviceManager> Create();
+
+  /// This struct will be returned by a manager for each device.
+  struct Device {
+    /// Must be set to true if device can be accessed by device file name
+    /// such as /dev/video0
+    bool accessed_by_name;
+    /// Device file name.
+    std::string name;
+    /// Device index.
+    int index;
+  };
+
+  /// Enumerate devices and return them to caller.
+  virtual std::vector<Device> GetDevices() const = 0;
+
+ private:
+  DISALLOW_COPY_AND_ASSIGN(DeviceManager);
+};
+
+}  // namespace core
+
+#endif  // CORE_DEVICE_MANAGER_H_
+
