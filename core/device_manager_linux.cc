4ba97de44603f49ce8b06187588a32f68a937d24
Vladislav <vlad.krikunoff@yandex.ru>
2014-11-14 21:44:28 +0300

    device name fix

--- core/device_manager_linux.cc
+++ core/device_manager_linux.cc
@@ -1,4 +1,4 @@
-// Copyright (c) 2014 The Caroline authors. All right reserved.
+// Copyright (c) 2014 The Caroline authors. All rights reserved.
 // Use of this source file is governed by a MIT license that can be found in the
 // LICENCE file.
 /// @author Vladislav Krikunov <vlad.krikunoff@yandex.ru>
@@ -7,10 +7,13 @@
 
 #include <fcntl.h>
 #include <unistd.h>
+#include <sys/ioctl.h>
+#include <linux/videodev2.h>
 
 namespace {
 
 const std::string ROOT_VIDEO = "/dev/video";
+const int MAX_DEVICES = 64;
 
 }
 
@@ -38,13 +41,17 @@ std::vector<DeviceManager::Device> LinuxDeviceManager::GetDevices() const {
   DeviceManager::Device device;
   int device_index = 0, descriptor;
   std::string path;
-  for (; ;) {
+  struct v4l2_capability params;
+  while (device_index <= MAX_DEVICES) {
     path = ROOT_VIDEO + std::to_string(device_index);
     descriptor = open(path.data(), O_RDONLY);
-    if (descriptor == -1) 
-      break;
+    if (descriptor == -1) {
+      ++device_index; 
+      continue;
+    }
+    ioctl(descriptor, VIDIOC_QUERYCAP, &params);
     device.accessed_by_name = true;
-    device.name = path;
+    device.name.assign(reinterpret_cast<char*>(params.card));
     device.index = device_index;
     devices.push_back(device);
     ++device_index;
